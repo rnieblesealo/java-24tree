a7b0a0f9da5ab057e803ef8bcf91415844b797fe
Rafael Niebles <rnieblesealo@gmail.com>
2024-10-02 11:09:42 -0400

    initial insertion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.lang.Integer;
+
 public class Main {
-    public static void main(String[] args){
-        TwoFourTree tree = new TwoFourTree();
+  public static int[] generateUniqueArray(int size, int min, int max) {
+    if (size > (max - min + 1)) {
+      throw new IllegalArgumentException("Range is too small for the requested array size.");
     }
+
+    Set<Integer> uniqueNumbers = new HashSet<>();
+    Random random = new Random();
+
+    // Keep adding unique random numbers to the set
+    while (uniqueNumbers.size() < size) {
+      int num = random.nextInt((max - min) + 1) + min;
+      uniqueNumbers.add(num);
+    }
+
+    // Convert set to array
+    int[] uniqueArray = new int[size];
+    int index = 0;
+    for (int num : uniqueNumbers) {
+      uniqueArray[index++] = num;
+    }
+
+    return uniqueArray;
+  }
+
+  public static void main(String[] args) {
+    TwoFourTree tree = new TwoFourTree();
+
+    System.out.print("-- CREATING TEST ARRAY --\n\n");
+
+    int[] nums = generateUniqueArray(8, 0, 32);
+   
+    for (int i = 0; i < 8; ++i) {
+      System.out.printf("%d ", nums[i]);
+    }
+
+    System.out.print("\n\n-- ADDING TO TREE --\n\n");
+   
+    for (int i = 0; i < 8; ++i){
+      tree.addValue(nums[i]);
+    }  
+
+    System.out.print("\n-- PRINTING TREE --\n\n");
+
+    tree.printInOrder();
+  }
 }
--- src/TwoFourTree.java
+++ src/TwoFourTree.java
@@ -1,19 +1,16 @@
 public class TwoFourTree {
   private class TwoFourTreeItem {
-    int values = 1;
+    int values = 0;
 
-    int value1 = 0; // always exists.
-    int value2 = 0; // exists iff the node is a 3-node or 4-node.
-    int value3 = 0; // exists iff the node is a 4-node.
+    int value1 = -1;
+    int value2 = -1;
+    int value3 = -1;
 
-    boolean isLeaf = true;
-
-    TwoFourTreeItem parent = null; // parent exists iff the node is not root.
-    TwoFourTreeItem leftChild = null; // left and right child exist iff the note is a non-leaf.
+    TwoFourTreeItem parent = null;
+    TwoFourTreeItem leftChild = null;
     TwoFourTreeItem rightChild = null;
-    TwoFourTreeItem centerChild = null; // center child exists iff the node is a non-leaf 3-node.
-    TwoFourTreeItem centerLeftChild = null; // center-left and center-right children exist iff the node is a non-leaf
-                                            // 4-node.
+    TwoFourTreeItem centerChild = null;
+    TwoFourTreeItem centerLeftChild = null;
     TwoFourTreeItem centerRightChild = null;
 
     public boolean isTwoNode() {
@@ -36,12 +33,16 @@ public class TwoFourTree {
       return this.parent == null;
     }
 
+    public boolean isLeaf() {
+      return leftChild == null && rightChild == null && centerChild == null && centerLeftChild == null
+          && centerRightChild == null;
+    }
+
     public TwoFourTreeItem(int value1) {
       // Creates a 2-node
+      this.values = 1;
 
       this.value1 = value1;
-      this.value2 = -1;
-      this.value3 = -1;
     }
 
     public TwoFourTreeItem(int value1, int value2) {
@@ -51,7 +52,6 @@ public class TwoFourTree {
 
       this.value1 = value1;
       this.value2 = value2;
-      this.value3 = -1;
     }
 
     public TwoFourTreeItem(int value1, int value2, int value3) {
@@ -72,34 +72,34 @@ public class TwoFourTree {
     }
 
     public void printInOrder(int indent) {
-      if (!isLeaf)
+      if (!isLeaf())
         leftChild.printInOrder(indent + 1);
       printIndents(indent);
       System.out.printf("%d\n", value1);
       if (isThreeNode()) {
-        if (!isLeaf)
+        if (!isLeaf())
           centerChild.printInOrder(indent + 1);
         printIndents(indent);
         System.out.printf("%d\n", value2);
       } else if (isFourNode()) {
-        if (!isLeaf)
+        if (!isLeaf())
           centerLeftChild.printInOrder(indent + 1);
         printIndents(indent);
         System.out.printf("%d\n", value2);
-        if (!isLeaf)
+        if (!isLeaf())
           centerRightChild.printInOrder(indent + 1);
         printIndents(indent);
         System.out.printf("%d\n", value3);
       }
-      if (!isLeaf)
+      if (!isLeaf())
         rightChild.printInOrder(indent + 1);
     }
   }
 
   TwoFourTreeItem root = null;
 
-  public boolean split(TwoFourTreeItem target){
-    if (!target.isFourNode()){
+  public boolean split(TwoFourTreeItem target) {
+    if (!target.isFourNode()) {
       return false;
     }
 
@@ -114,6 +114,8 @@ public class TwoFourTree {
     target.value2 = -1;
     target.value3 = -1;
 
+    target.values = 1;
+
     // Relink children of 4-node to newly created nodes
 
     newLeftChild.leftChild = target.leftChild;
@@ -122,7 +124,8 @@ public class TwoFourTree {
     newRightChild.leftChild = target.centerRightChild;
     newRightChild.rightChild = target.rightChild;
 
-    // Finalize making the target into a 2-node, childed with the newly created nodes
+    // Finalize making the target into a 2-node, childed with the newly created
+    // nodes
 
     target.leftChild = newLeftChild;
     target.rightChild = newRightChild;
@@ -135,7 +138,219 @@ public class TwoFourTree {
   }
 
   public boolean addValue(int value) {
-    return false;
+    // Case: We have no root! Create one
+
+    if (root == null) {
+      root = new TwoFourTreeItem(value);
+
+      System.out.printf("No root; creating new: [ %d | %d | %d ]\n", root.value1, root.value2, root.value3);
+
+      System.out.printf("Finished inserting %d\n", value);
+      
+      return false;
+    }
+
+    // Otherwise, begin walking down the tree
+
+    TwoFourTreeItem current = root;
+
+    while (current != null) {
+      // If we stumble upon a 4-node, split it!
+
+      if (current.isFourNode()) {
+        System.out.printf("At 4-node [ %d | %d | %d ]; splitting...\n", current.value1, current.value2, current.value3);
+
+        split(current);
+      }
+
+      // Case: We hit a leaf node, so we may try inserting at it
+
+      if (current.isLeaf()) {
+        System.out.printf("Hit leaf node [ %d | %d | %d ]; ", current.value1, current.value2, current.value3);
+
+        if (current.isTwoNode()) {
+
+          if (value < current.value1) {
+            current.value2 = current.value1;
+            current.value1 = value;
+          }
+
+          else {
+            current.value2 = value;
+          }
+
+          System.out.printf("after insert: [ %d | %d | %d ]\n", current.value1, current.value2, current.value3);
+
+        }
+
+        else if (current.isThreeNode()) {
+
+          if (value < current.value1) {
+            current.value3 = current.value2;
+            current.value2 = current.value1;
+            current.value1 = value;
+          }
+
+          else if (value >= current.value1 && value <= current.value2) {
+            current.value3 = current.value2;
+            current.value2 = value;
+          }
+
+          else {
+            current.value3 = value;
+          }
+
+          System.out.printf("after insert: [ %d | %d | %d ]\n", current.value1, current.value2, current.value3);
+        }
+
+        // Adding a value increases node value count!
+
+        current.values++;
+      }
+
+      // Move further down the tree
+
+      if (current.isTwoNode()) {
+
+        System.out.printf("Moving from 2-node [ %d | %d | %d ] to ", current.value1, current.value2, current.value3);
+
+        if (value < current.value1) {
+          current = current.leftChild;
+
+          if (current != null) {
+            System.out.printf("left child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("left child (NULL)\n");
+          }
+
+        }
+
+        else {
+          current = current.rightChild;
+
+          if (current != null) {
+            System.out.printf("right child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("right child (NULL)\n");
+          }
+
+        }
+
+      }
+
+      else if (current.isThreeNode()) {
+
+        System.out.printf("Moving from 3-node [ %d | %d | %d ] to ", current.value1, current.value2, current.value3);
+
+        if (value < current.value1) {
+          current = current.leftChild;
+
+          if (current != null) {
+            System.out.printf("left child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("left child (NULL)\n");
+          }
+
+        }
+
+        else if (value >= current.value1 && value <= current.value2) {
+          current = current.centerChild;
+
+          if (current != null) {
+            System.out.printf("center child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("center child (NULL)\n");
+          }
+
+        }
+
+        else {
+          current = current.rightChild;
+
+          if (current != null) {
+            System.out.printf("right child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("right child (NULL)\n");
+          }
+
+        }
+
+      }
+
+      else if (current.isFourNode()) {
+
+        System.out.printf("Moving from 4-node [ %d | %d | %d ] to ", current.value1, current.value2, current.value3);
+
+        if (value < current.value1) {
+          current = current.leftChild;
+
+
+          if (current != null) {
+            System.out.printf("left child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("left child (NULL)\n");
+          }
+        
+        }
+
+        else if (value >= current.value1 && value < current.value2) {
+          current = current.centerLeftChild;
+
+          if (current != null) {
+            System.out.printf("center left child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("center left child (NULL)\n");
+          }
+
+        }
+
+        else if (value >= current.value2 && value <= current.value3) {
+          current = current.centerRightChild;
+
+          if (current != null) {
+            System.out.printf("center right child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("center right child (NULL)\n");
+          }
+
+        }
+
+        else {
+          current = current.rightChild;
+          
+          if (current != null) {
+            System.out.printf("right child [ %d | %d | %d]\n", current.value1, current.value2, current.value3);
+          }
+
+          else {
+            System.out.print("right child (NULL)\n");
+          }
+        
+        }
+
+      }
+    
+    }
+
+    System.out.printf("Finished inserting %d\n", value);
+
+    return true;
   }
 
   public boolean hasValue(int value) {
@@ -153,8 +368,8 @@ public class TwoFourTree {
       root.printInOrder(0);
   }
 
-  private void printTreeItem(TwoFourTreeItem item){
-    if (item == null){
+  private void printTreeItem(TwoFourTreeItem item) {
+    if (item == null) {
       System.out.print("[ NO NODE ]");
       return;
     }
@@ -162,10 +377,10 @@ public class TwoFourTree {
     System.out.printf("[ %d | %d | %d ]", item.value1, item.value2, item.value3);
   }
 
-  private void splitTest(){
+  private void splitTest() {
     // Create sample node and give it children
 
-    TwoFourTreeItem test = new TwoFourTreeItem(1, 2, 3);
+    TwoFourTreeItem test = new TwoFourTreeItem(1, 2);
 
     test.leftChild = new TwoFourTreeItem(1, 2, 3);
     test.centerLeftChild = new TwoFourTreeItem(3);
@@ -176,19 +391,37 @@ public class TwoFourTree {
 
     System.out.println("--- BEFORE ---\n");
 
-    System.out.print("TARGET: "); printTreeItem(test); System.out.println("\n");
+    System.out.print("TARGET: ");
+    printTreeItem(test);
+    System.out.println("\n");
 
-    System.out.print("L: "); printTreeItem(test.leftChild); System.out.print("\n");
-    System.out.print("CL: "); printTreeItem(test.centerLeftChild); System.out.print("\n");
-    System.out.print("CR: "); printTreeItem(test.centerRightChild); System.out.print("\n");
-    System.out.print("R: "); printTreeItem(test.rightChild); System.out.print("\n");
+    System.out.print("L: ");
+    printTreeItem(test.leftChild);
+    System.out.print("\n");
+    System.out.print("CL: ");
+    printTreeItem(test.centerLeftChild);
+    System.out.print("\n");
+    System.out.print("CR: ");
+    printTreeItem(test.centerRightChild);
+    System.out.print("\n");
+    System.out.print("R: ");
+    printTreeItem(test.rightChild);
+    System.out.print("\n");
 
     System.out.print("\n");
 
-    System.out.print("L of L: "); printTreeItem(test.leftChild.leftChild); System.out.print("\n");
-    System.out.print("R of L: "); printTreeItem(test.leftChild.rightChild); System.out.print("\n");
-    System.out.print("L of R: "); printTreeItem(test.rightChild.leftChild); System.out.print("\n");
-    System.out.print("R of R: "); printTreeItem(test.rightChild.rightChild); System.out.print("\n");
+    System.out.print("L of L: ");
+    printTreeItem(test.leftChild.leftChild);
+    System.out.print("\n");
+    System.out.print("R of L: ");
+    printTreeItem(test.leftChild.rightChild);
+    System.out.print("\n");
+    System.out.print("L of R: ");
+    printTreeItem(test.rightChild.leftChild);
+    System.out.print("\n");
+    System.out.print("R of R: ");
+    printTreeItem(test.rightChild.rightChild);
+    System.out.print("\n");
 
     // Perform test split
 
@@ -198,22 +431,40 @@ public class TwoFourTree {
 
     System.out.println("\n--- AFTER ---\n");
 
-    System.out.print("TARGET: "); printTreeItem(test); System.out.println("\n");
+    System.out.print("TARGET: ");
+    printTreeItem(test);
+    System.out.println("\n");
 
-    System.out.print("L: "); printTreeItem(test.leftChild); System.out.print("\n");
-    System.out.print("CL: "); printTreeItem(test.centerLeftChild); System.out.print("\n");
-    System.out.print("CR: "); printTreeItem(test.centerRightChild); System.out.print("\n");
-    System.out.print("R: "); printTreeItem(test.rightChild); System.out.print("\n");
+    System.out.print("L: ");
+    printTreeItem(test.leftChild);
+    System.out.print("\n");
+    System.out.print("CL: ");
+    printTreeItem(test.centerLeftChild);
+    System.out.print("\n");
+    System.out.print("CR: ");
+    printTreeItem(test.centerRightChild);
+    System.out.print("\n");
+    System.out.print("R: ");
+    printTreeItem(test.rightChild);
+    System.out.print("\n");
 
     System.out.print("\n");
 
-    System.out.print("L of L: "); printTreeItem(test.leftChild.leftChild); System.out.print("\n");
-    System.out.print("R of L: "); printTreeItem(test.leftChild.rightChild); System.out.print("\n");
-    System.out.print("L of R: "); printTreeItem(test.rightChild.leftChild); System.out.print("\n");
-    System.out.print("R of R: "); printTreeItem(test.rightChild.rightChild); System.out.print("\n");
+    System.out.print("L of L: ");
+    printTreeItem(test.leftChild.leftChild);
+    System.out.print("\n");
+    System.out.print("R of L: ");
+    printTreeItem(test.leftChild.rightChild);
+    System.out.print("\n");
+    System.out.print("L of R: ");
+    printTreeItem(test.rightChild.leftChild);
+    System.out.print("\n");
+    System.out.print("R of R: ");
+    printTreeItem(test.rightChild.rightChild);
+    System.out.print("\n");
   }
 
   public TwoFourTree() {
-    splitTest();
+
   }
 }
